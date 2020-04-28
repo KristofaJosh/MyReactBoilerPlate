1e64265a900276dc4e2244508a4a77bf62a0ab28
KristofaJosh <christopherjoshua18@gmail.com>
2020-04-28 22:32:37 +0100

    install graphql

--- src/ui/composite/pages/authentication/index.jsx
+++ src/ui/composite/pages/authentication/index.jsx
@@ -5,8 +5,8 @@ import {Link} from "react-router-dom";
 const AuthPage = () => {
     return <>
         <AuthTemplate>
-            <p>Login or SignUp</p>
-            <Link to={'/'}> Home </Link>
+            <p>Auth Page</p>
+            <Link to={'/'}> Goto Home Page </Link>
         </AuthTemplate>
     </>
 };
