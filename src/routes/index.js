623517ad61dc4711f0cc771cbbf90a34a0352584
KristofaJosh <christopherjoshua18@gmail.com>
2020-05-21 00:21:43 +0100

    change name

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,8 @@
 import React, {lazy, Suspense} from "react";
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import NotFound from "../ui/composite/pages/errorpages/404";
 import ExchangeRates from "../ui/composite/pages/appollo";
+
 
 // noinspection JSCheckFunctionSignatures
 const AuthPage = lazy(() => import("../ui/composite/pages/authentication"));
@@ -16,14 +17,14 @@ function AppRoutes() {
             <Suspense fallback={
                 'loading...'
             }>
-                <Router>
+                <BrowserRouter>
                     <Switch>
                         <Route exact path="/" component={HomePage}/>
-                        <Route path={'/appollo'} component={ExchangeRates}/>
+                        <Route path={'/apollo'} component={ExchangeRates}/>
                         <Route path="/auth" component={AuthPage}/>
                         <Route path="*" component={NotFound}/>
                     </Switch>
-                </Router>
+                </BrowserRouter>
             </Suspense>
         </>
     );
