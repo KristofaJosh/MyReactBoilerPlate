7215ac27867c26e480f05ae65ecf0cdbacdb7b85
Restyled.io <commits@restyled.io>
2020-05-21 13:44:32 +0000

    Restyled by clang-format

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,33 +1,29 @@
 import React, {lazy, Suspense} from "react";
 import {BrowserRouter, Route, Switch} from "react-router-dom";
-import NotFound from "../ui/composite/pages/errorpages/404";
-import ExchangeRates from "../ui/composite/pages/appollo";
 
+import ExchangeRates from "../ui/composite/pages/appollo";
+import NotFound from "../ui/composite/pages/errorpages/404";
 
 // noinspection JSCheckFunctionSignatures
 const AuthPage = lazy(() => import("../ui/composite/pages/authentication"));
 // noinspection JSCheckFunctionSignatures
 const HomePage = lazy(() => import("../ui/composite/pages/home"));
 
-
 function AppRoutes() {
-    
-    return (
-        <>
-            <Suspense fallback={
-                'loading...'
-            }>
-                <BrowserRouter>
-                    <Switch>
-                        <Route exact path="/" component={HomePage}/>
-                        <Route path={'/apollo'} component={ExchangeRates}/>
-                        <Route path="/auth" component={AuthPage}/>
+
+  return (
+      <><Suspense fallback = {'loading...'}><BrowserRouter><Switch><
+          Route exact path = "/" component =
+      { HomePage } />
+                        <Route path={'/apollo'} component={ExchangeRates}/> <
+          Route path = "/auth" component =
+          {
+            AuthPage
+          } />
                         <Route path="*" component={NotFound}/>
-                    </Switch>
-                </BrowserRouter>
-            </Suspense>
-        </>
-    );
+      </Switch>
+                </BrowserRouter></Suspense>
+        </>);
 }
 
 export default AppRoutes;
